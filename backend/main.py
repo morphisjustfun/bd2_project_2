817808f8afc31b796486cf0adcc1b99b69ebd896
Mario.Rios <qvoiekpjzzvjepg35rrsarhcyx2rcx3y5kf6oa7ygz6bycfyfvyq>
2023-06-14 19:04:49 -0500

    :sparkles; avnace

--- backend/main.py
+++ backend/main.py
@@ -2,4 +2,6 @@ from src.spimi_inverted_index import SPIMIInvertedIndex
 
 if __name__ == "__main__":
     test = SPIMIInvertedIndex(data_file_name="data/data.json", output_dir="dist/", block_size=50000)
-    test.build()
+    # test.clean()
+    # test.build()
+    test.merge_blocks()
--- backend/src/spimi_inverted_index.py
+++ backend/src/spimi_inverted_index.py
@@ -1,3 +1,4 @@
+import glob
 import json
 import math
 import os
@@ -17,12 +18,14 @@ class SPIMIInvertedIndex:
         # nltk.download('punkt')
         # nltk.download('stopwords')
         self.output_dir = output_dir
-        if os.path.exists(self.output_dir):
-            shutil.rmtree(self.output_dir)
-        os.makedirs(self.output_dir)
         self.dataFileName = data_file_name
         self.blockSize = block_size
         self.stop_words = set(nltk.corpus.stopwords.words('english'))
+
+    def clean(self):
+        if os.path.exists(self.output_dir):
+            shutil.rmtree(self.output_dir)
+        os.makedirs(self.output_dir)
 
     def build(self) -> None:
         data = open(self.dataFileName)
@@ -80,8 +83,9 @@ class SPIMIInvertedIndex:
         df['posting_list'] = df['posting_list'].apply(lambda x: json.dumps(x))
         df.to_feather(output_file_path)
 
-    def read_bloc_from_disk(self, temp_index: int) -> pd.DataFrame:
-        df = pd.read_feather(f'{self.output_dir}/temp_{temp_index}.feather')
+    @staticmethod
+    def read_bloc_from_disk(file_path: str) -> pd.DataFrame:
+        df = pd.read_feather(file_path)
         df.set_index('word', inplace=True)
         df['posting_list'] = df['posting_list'].apply(lambda x: json.loads(x))
         return df
@@ -91,7 +95,65 @@ class SPIMIInvertedIndex:
         # merge blocks. Use BSBI algorithm
         # blocks = [glob.glob(f'{self.output_dir}/temp_{i}.feather') for i in range(1, 5)]
         # levels = math.log2(self.blockSize)
-        # initial_simultaneous_blocks = 2
-        pass
 
+        # total_block_files are all files in the dist/ directory
+        total_blocks_files = glob.glob(f'{self.output_dir}/*.feather')
+        total_current_batch_size = 2
 
+        while total_current_batch_size != len(total_blocks_files):
+            # create folder dist/totals_current_batch_size
+            output_folder = f'{self.output_dir}/{total_current_batch_size}'
+
+            if os.path.exists(output_folder):
+                shutil.rmtree(output_folder)
+            os.makedirs(output_folder)
+
+            remainder = len(total_blocks_files) % total_current_batch_size
+            if remainder != 0:
+                total_blocks_files.extend([total_blocks_files[-1]] * (total_current_batch_size - remainder))
+
+            for i in range(0, len(total_blocks_files), total_current_batch_size):
+                # in this point total_current_batch_size files output must be created
+                first_group = total_blocks_files[i:i + total_current_batch_size // 2]
+                # first_group is the first half of the total_current_batch_size files
+                second_group = total_blocks_files[i + total_current_batch_size // 2:i + total_current_batch_size]
+                # second_group is the second half of the total_current_batch_size files
+                files_written = 0
+
+                # bsbi algorithm
+                while len(first_group) > 0 or len(second_group) > 0:
+                    first_buffer: None | pd.DataFrame = None
+                    second_buffer: None | pd.DataFrame = None
+                    output_buffer: pd.DataFrame
+                    if len(first_group) > 0:
+                        first_buffer = self.read_bloc_from_disk(first_group[0])
+                        output_buffer = first_buffer.iloc[0:0].copy()
+                    if len(second_group) > 0:
+                        second_buffer = self.read_bloc_from_disk(second_group[0])
+                        output_buffer = second_buffer.iloc[0:0].copy()
+
+                    while len(first_buffer) > 0 and len(second_buffer) > 0:
+                        first_word = first_buffer.index[0]
+                        second_word = second_buffer.index[0]
+
+                        # first first_word is smaller than second_word, then insert first_word into output_buffer
+                        # and remove first_word from first_buffer
+                        # if first_word is equal to second_word, then merge the posting lists of first_word and second_word
+                        # and insert the merged posting list into output_buffer and remove first_word and second_word from
+
+                        if first_word < second_word:
+                            output_buffer = output_buffer.append(first_buffer.iloc[0])
+                            first_buffer.drop(first_word, inplace=True)
+                        elif first_word > second_word:
+                            output_buffer = output_buffer.append(second_buffer.iloc[0])
+                            second_buffer.drop(second_word, inplace=True)
+                        else:
+                            # merge posting lists, the index of the posting list is the id of the document
+                            # the value of the posting list is the frequency of the word in the document
+                            merged_posting_list = first_buffer.loc[first_word, 'posting_list'] + \
+                                                  second_buffer.loc[second_word, 'posting_list']
+                            # append (index-first_word, value-merged_posting_list) to output_buffer
+                            output_buffer = output_buffer.append(pd.DataFrame({'posting_list': [merged_posting_list]},
+                                                                              index=[first_word]))
+                            first_buffer.drop(first_word, inplace=True)
+                            second_buffer.drop(second_word, inplace=True)
