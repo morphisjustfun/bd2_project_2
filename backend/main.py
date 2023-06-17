8f4a854816f70e46bc3fcfafdf5398fd44927d0e
Mario.Rios <qvoiekpjzzvjepg35rrsarhcyx2rcx3y5kf6oa7ygz6bycfyfvyq>
2023-06-16 23:48:39 -0500

    ✨ benchmark

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,17 @@
+import time
+
 from src.spimi_inverted_index import SPIMIInvertedIndex
+from multiprocessing import cpu_count
 
 if __name__ == "__main__":
     test = SPIMIInvertedIndex(data_file_name="data/data.json", output_dir="dist/", block_size=50000)
     # test.clean()
     # test.build()
+    # benchmark with miliseconds precision
+    begin = time.time()
     test.merge_blocks()
+    end = time.time()
+    print("Cores", cpu_count())
+    print("Time elapsed: ", end - begin, "s")
+    print("Time elapsed: ", (end - begin) * 1000, "ms")
+
