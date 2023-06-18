06d9ec10cc78f32b61868a2cab961bf37197f5ab
Mario.Rios <qvoiekpjzzvjepg35rrsarhcyx2rcx3y5kf6oa7ygz6bycfyfvyq>
2023-06-17 19:49:42 -0500

    :sparkles: liompiueza

--- backend/main.py
+++ backend/main.py
@@ -5,13 +5,12 @@ from multiprocessing import cpu_count
 
 if __name__ == "__main__":
     test = SPIMIInvertedIndex(data_file_name="data/data.json", output_dir="dist/", block_size=50000)
-    # test.clean()
+    test.clean()
     begin = time.time()
     test.build()
     end = time.time()
     print("Cores", cpu_count())
     print("Time elapsed: ", end - begin, "s")
     print("Time elapsed: ", (end - begin) * 1000, "ms")
-    # benchmark with miliseconds precision
     test.merge_blocks()
 
